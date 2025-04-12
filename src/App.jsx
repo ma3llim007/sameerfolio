10fb72ddf3a926275ad589aca199a5c153742cfa
ma3llim007 <mohdsameer68257@gmail.com>
2025-04-12 23:30:05 +0530

    feat: add logo and favicon; update the title

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,7 @@ import React from "react";
 const App = () => {
     return (
         <div className="w-screen h-screen bg-slate-900 text-white">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos similique necessitatibus veritatis, corporis nihil magnam laborum sed in hic commodi optio harum quibusdam itaque
-            expedita enim et numquam aperiam ad.
+            <Header/>
         </div>
     );
 };
