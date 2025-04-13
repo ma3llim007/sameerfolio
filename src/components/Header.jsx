44b53660f452011446ab0b54461ff0d11b057ca3
ma3llim007 <mohdsameer68257@gmail.com>
2025-04-13 10:26:24 +0530

    style: update the height of the header section

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,14 +2,14 @@ import { Command } from "lucide-react";
 
 const Header = () => {
     return (
-        <div className="fixed top-0 left-0 right-0 z-50 bg-black/20 backdrop-blur-sm border-b border-white/10 shadow-md shadow-gray-700 transition-all duration-300">
-            <div className="grid grid-cols-2 px-6 md:px-12 h-20 max-w-full mx-auto">
+        <div className="fixed top-0 left-0 right-0 z-50 bg-black/10 backdrop-blur-sm border-b border-white/10 shadow-md shadow-gray-700 transition-all duration-300">
+            <div className="grid grid-cols-2 items-center px-6 md:px-12 h-14 max-w-full mx-auto">
                 {/* Logo Section */}
-                <div className="w-[200px] h-[55px]">
+                <div className="w-[160px] h-[40px] flex items-center">
                     <img className="w-full h-auto object-contain" src="logo.svg" alt="Sameerfolio Logo" />
                 </div>
                 <nav className="flex items-center justify-end gap-6">
-                    <ul className="hidden md:flex items-center gap-6 font-semibold text-lg text-dark-text">
+                    <ul className="hidden md:flex items-center gap-6 font-semibold text-lg">
                         <li className="hover:text-primary transition">Home</li>
                         <li className="hover:text-primary transition">Skills</li>
                         <li className="hover:text-primary transition">Project's</li>
