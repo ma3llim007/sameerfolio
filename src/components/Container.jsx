1b208cf9031a485c54af5071390133effa839ea0
ma3llim007 <mohdsameer68257@gmail.com>
2025-04-13 10:35:29 +0530

    style: update container style

--- src/components/Container.jsx
+++ src/components/Container.jsx
@@ -1,5 +1,5 @@
 const Container = ({ children }) => {
-    return <div className="w-full max-w-7xl mx-auto px-4">{children}</div>;
+    return <div className="container mx-auto px-4 sm:px-6 lg:px-8">{children}</div>;
 };
 
 export default Container;
