ccb301a62f0d9b09bf28a692593a20605b1bd30a
ma3llim007 <mohdsameer68257@gmail.com>
2025-04-14 10:45:56 +0530

    style: set here skeleton responsive for all device

--- src/components/skeleton/Hero.jsx
+++ src/components/skeleton/Hero.jsx
@@ -1,15 +1,17 @@
+import Container from "../Container";
+
 const SkeletonHero = () => {
     return (
         <div class="w-full bg-gradient-to-r from-purple-900 via-indigo-900 to-blue-900 text-white backdrop-blur-md" id="home">
             <div class="w-full bg-black/50">
-                <div className="container mx-auto lg:px-10">
+                <Container>
                     <div class="flex flex-col-reverse md:flex-row items-center justify-center text-center lg:text-start gap-12 py-5">
-                        <div class="space-y-4 animate-pulse grow">
-                            <h2 class="h-6 bg-white rounded w-1/2"></h2>
-                            <div class="h-10 bg-white rounded w-2/3"></div>
-                            <p class="h-4 bg-white rounded w-2/3"></p>
-                            <p class="h-4 bg-white rounded w-2/3"></p>
-                            <p class="h-4 bg-white rounded w-60"></p>
+                        <div class="space-y-4 animate-pulse grow text-center">
+                            <h2 class="h-6 bg-white rounded lg:w-1/2"></h2>
+                            <div class="h-10 bg-white rounded lg:w-2/3"></div>
+                            <p class="h-4 bg-white rounded lg:w-2/3"></p>
+                            <p class="h-4 bg-white rounded lg:w-2/3"></p>
+                            <p class="h-4 bg-white rounded lg:w-60"></p>
                             <div class="flex flex-wrap space-x-4 items-center justify-center lg:justify-start">
                                 <div class="h-10 bg-white rounded w-32"></div>
                                 <div class="w-10 h-10 rounded-full bg-white"></div>
@@ -21,7 +23,7 @@ const SkeletonHero = () => {
                             <div class="w-40 h-40 md:w-64 md:h-64 object-cover rounded-full border-4 border-white shadow-xl bg-white"></div>
                         </div>
                     </div>
-                </div>
+                </Container>
             </div>
         </div>
     );
