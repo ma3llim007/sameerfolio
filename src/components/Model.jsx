caf0c57ea944b4747c9875edef0a3333b7d4afb6
ma3llim007 <mohdsameer68257@gmail.com>
2025-04-14 16:28:40 +0530

    style: make a model responsive of all device; and max height of model; set scroll

--- src/components/Model.jsx
+++ src/components/Model.jsx
@@ -16,18 +16,19 @@ const Model = ({ isOpen, onClose, title, children }) => {
 
         return () => document.removeEventListener("keydown", handleKeyDown);
     }, [isOpen, onClose]);
+
     return createPortal(
         <AnimatePresence>
             {isOpen && (
                 <motion.div
-                    className="fixed inset-0 z-50 flex items-center justify-center bg-black/50 backdrop-blur-md"
+                    className="fixed inset-0 z-50 flex items-center justify-center bg-black/50 backdrop-blur-md overflow-hidden"
                     initial={{ opacity: 0 }}
                     animate={{ opacity: 1 }}
                     exit={{ opacity: 0 }}
                     onClick={onClose}
                 >
                     <motion.div
-                        className="bg-gray-900 text-white p-6 rounded-xl max-w-7xl w-full shadow-2xl relative"
+                        className="bg-gray-900 text-white p-6 rounded-xl max-w-sm sm:max-w-2xl md:max-w-3xl lg:max-w-4xl xl:max-w-7xl w-full shadow-2xl relative"
                         initial={{ scale: 0.9, opacity: 0 }}
                         animate={{ scale: 1, opacity: 1 }}
                         exit={{ scale: 0.9, opacity: 0 }}
@@ -38,7 +39,7 @@ const Model = ({ isOpen, onClose, title, children }) => {
                             &times;
                         </button>
                         {title && <h2 className="text-2xl font-bold mb-4 border-b border-white/10 pb-2">{title}</h2>}
-                        <div className="text-gray-300">{children}</div>
+                        <div className="max-h-[60vh] overflow-y-scroll">{children}</div>
                     </motion.div>
                 </motion.div>
             )}
