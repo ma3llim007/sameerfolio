84bf7d5afd8dd1756a9208800568a33d640dabe9
ma3llim007 <mohdsameer68257@gmail.com>
2025-04-14 21:32:46 +0530

    refactor: update class attribute to className

--- src/components/skeleton/Hero.jsx
+++ src/components/skeleton/Hero.jsx
@@ -2,25 +2,25 @@ import Container from "../Container";
 
 const SkeletonHero = () => {
     return (
-        <div class="w-full bg-gradient-to-r from-purple-900 via-indigo-900 to-blue-900 text-white backdrop-blur-md" id="home">
-            <div class="w-full bg-black/50">
+        <div className="w-full bg-gradient-to-r from-purple-900 via-indigo-900 to-blue-900 text-white backdrop-blur-md" id="home">
+            <div className="w-full bg-black/50">
                 <Container>
-                    <div class="flex flex-col-reverse md:flex-row items-center justify-center text-center lg:text-start gap-12 py-5">
-                        <div class="space-y-4 animate-pulse grow text-center">
-                            <h2 class="h-6 bg-white rounded lg:w-1/2"></h2>
-                            <div class="h-10 bg-white rounded lg:w-2/3"></div>
-                            <p class="h-4 bg-white rounded lg:w-2/3"></p>
-                            <p class="h-4 bg-white rounded lg:w-2/3"></p>
-                            <p class="h-4 bg-white rounded lg:w-60"></p>
-                            <div class="flex flex-wrap space-x-4 items-center justify-center lg:justify-start">
-                                <div class="h-10 bg-white rounded w-32"></div>
-                                <div class="w-10 h-10 rounded-full bg-white"></div>
-                                <div class="w-10 h-10 rounded-full bg-white"></div>
-                                <div class="w-10 h-10 rounded-full bg-white"></div>
+                    <div className="flex flex-col-reverse md:flex-row items-center justify-center text-center lg:text-start gap-12 py-5">
+                        <div className="space-y-4 animate-pulse grow text-center">
+                            <h2 className="h-6 bg-white rounded lg:w-1/2"></h2>
+                            <div className="h-10 bg-white rounded lg:w-2/3"></div>
+                            <p className="h-4 bg-white rounded lg:w-2/3"></p>
+                            <p className="h-4 bg-white rounded lg:w-2/3"></p>
+                            <p className="h-4 bg-white rounded lg:w-60"></p>
+                            <div className="flex flex-wrap space-x-4 items-center justify-center lg:justify-start">
+                                <div className="h-10 bg-white rounded w-32"></div>
+                                <div className="w-10 h-10 rounded-full bg-white"></div>
+                                <div className="w-10 h-10 rounded-full bg-white"></div>
+                                <div className="w-10 h-10 rounded-full bg-white"></div>
                             </div>
                         </div>
-                        <div class="w-full max-w-xs mx-auto md:mx-0 flex justify-center md:justify-end">
-                            <div class="w-40 h-40 md:w-64 md:h-64 object-cover rounded-full border-4 border-white shadow-xl bg-white"></div>
+                        <div className="w-full max-w-xs mx-auto md:mx-0 flex justify-center md:justify-end">
+                            <div className="w-40 h-40 md:w-64 md:h-64 object-cover rounded-full border-4 border-white shadow-xl bg-white"></div>
                         </div>
                     </div>
                 </Container>
--- src/components/skeleton/Skills.jsx
+++ src/components/skeleton/Skills.jsx
@@ -3,61 +3,61 @@ import Container from "../Container";
 
 const SkeletonSkills = () => {
     return (
-        <section class="w-full py-7" id="skills">
+        <section className="w-full py-7" id="skills">
             <Container>
-                <h3 class="text-2xl font-extrabold mb-3 animate-pulse underline">
-                    <div class="h-6 bg-gray-200 rounded w-1/4"></div>
+                <h3 className="text-2xl font-extrabold mb-3 animate-pulse underline">
+                    <div className="h-6 bg-gray-200 rounded w-1/4"></div>
                 </h3>
-                <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
-                    <div class="w-full space-y-4 p-5 rounded-xl bg-white/10 backdrop-blur-md ring-1 ring-white/10 shadow-lg animate-pulse">
-                        <div class="h-6 bg-gray-200 rounded w-1/3"></div>
-                        <div class="flex flex-wrap gap-2">
-                            <div class="bg-gray-100/50 text-white text-sm px-3 py-1.5 font-medium rounded-full backdrop-blur-sm shadow hover:bg-indigo-700 transition duration-200 select-none h-5 w-20"></div>
-                            <div class="bg-gray-100/50 text-white text-sm px-3 py-1.5 font-medium rounded-full backdrop-blur-sm shadow hover:bg-indigo-700 transition duration-200 select-none h-5 w-20"></div>
-                            <div class="bg-gray-100/50 text-white text-sm px-3 py-1.5 font-medium rounded-full backdrop-blur-sm shadow hover:bg-indigo-700 transition duration-200 select-none h-5 w-20"></div>
-                            <div class="bg-gray-100/50 text-white text-sm px-3 py-1.5 font-medium rounded-full backdrop-blur-sm shadow hover:bg-indigo-700 transition duration-200 select-none h-5 w-20"></div>
-                            <div class="bg-gray-100/50 text-white text-sm px-3 py-1.5 font-medium rounded-full backdrop-blur-sm shadow hover:bg-indigo-700 transition duration-200 select-none h-5 w-20"></div>
-                            <div class="bg-gray-100/50 text-white text-sm px-3 py-1.5 font-medium rounded-full backdrop-blur-sm shadow hover:bg-indigo-700 transition duration-200 select-none h-5 w-20"></div>
-                            <div class="bg-gray-100/50 text-white text-sm px-3 py-1.5 font-medium rounded-full backdrop-blur-sm shadow hover:bg-indigo-700 transition duration-200 select-none h-5 w-20"></div>
-                            <div class="bg-gray-100/50 text-white text-sm px-3 py-1.5 font-medium rounded-full backdrop-blur-sm shadow hover:bg-indigo-700 transition duration-200 select-none h-5 w-20"></div>
-                            <div class="bg-gray-100/50 text-white text-sm px-3 py-1.5 font-medium rounded-full backdrop-blur-sm shadow hover:bg-indigo-700 transition duration-200 select-none h-5 w-20"></div>
-                            <div class="bg-gray-100/50 text-white text-sm px-3 py-1.5 font-medium rounded-full backdrop-blur-sm shadow hover:bg-indigo-700 transition duration-200 select-none h-5 w-20"></div>
-                            <div class="bg-gray-100/50 text-white text-sm px-3 py-1.5 font-medium rounded-full backdrop-blur-sm shadow hover:bg-indigo-700 transition duration-200 select-none h-5 w-20"></div>
-                            <div class="bg-gray-100/50 text-white text-sm px-3 py-1.5 font-medium rounded-full backdrop-blur-sm shadow hover:bg-indigo-700 transition duration-200 select-none h-5 w-20"></div>
+                <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+                    <div className="w-full space-y-4 p-5 rounded-xl bg-white/10 backdrop-blur-md ring-1 ring-white/10 shadow-lg animate-pulse">
+                        <div className="h-6 bg-gray-200 rounded w-1/3"></div>
+                        <div className="flex flex-wrap gap-2">
+                            <div className="bg-gray-100/50 text-white text-sm px-3 py-1.5 font-medium rounded-full backdrop-blur-sm shadow hover:bg-indigo-700 transition duration-200 select-none h-5 w-20"></div>
+                            <div className="bg-gray-100/50 text-white text-sm px-3 py-1.5 font-medium rounded-full backdrop-blur-sm shadow hover:bg-indigo-700 transition duration-200 select-none h-5 w-20"></div>
+                            <div className="bg-gray-100/50 text-white text-sm px-3 py-1.5 font-medium rounded-full backdrop-blur-sm shadow hover:bg-indigo-700 transition duration-200 select-none h-5 w-20"></div>
+                            <div className="bg-gray-100/50 text-white text-sm px-3 py-1.5 font-medium rounded-full backdrop-blur-sm shadow hover:bg-indigo-700 transition duration-200 select-none h-5 w-20"></div>
+                            <div className="bg-gray-100/50 text-white text-sm px-3 py-1.5 font-medium rounded-full backdrop-blur-sm shadow hover:bg-indigo-700 transition duration-200 select-none h-5 w-20"></div>
+                            <div className="bg-gray-100/50 text-white text-sm px-3 py-1.5 font-medium rounded-full backdrop-blur-sm shadow hover:bg-indigo-700 transition duration-200 select-none h-5 w-20"></div>
+                            <div className="bg-gray-100/50 text-white text-sm px-3 py-1.5 font-medium rounded-full backdrop-blur-sm shadow hover:bg-indigo-700 transition duration-200 select-none h-5 w-20"></div>
+                            <div className="bg-gray-100/50 text-white text-sm px-3 py-1.5 font-medium rounded-full backdrop-blur-sm shadow hover:bg-indigo-700 transition duration-200 select-none h-5 w-20"></div>
+                            <div className="bg-gray-100/50 text-white text-sm px-3 py-1.5 font-medium rounded-full backdrop-blur-sm shadow hover:bg-indigo-700 transition duration-200 select-none h-5 w-20"></div>
+                            <div className="bg-gray-100/50 text-white text-sm px-3 py-1.5 font-medium rounded-full backdrop-blur-sm shadow hover:bg-indigo-700 transition duration-200 select-none h-5 w-20"></div>
+                            <div className="bg-gray-100/50 text-white text-sm px-3 py-1.5 font-medium rounded-full backdrop-blur-sm shadow hover:bg-indigo-700 transition duration-200 select-none h-5 w-20"></div>
+                            <div className="bg-gray-100/50 text-white text-sm px-3 py-1.5 font-medium rounded-full backdrop-blur-sm shadow hover:bg-indigo-700 transition duration-200 select-none h-5 w-20"></div>
                         </div>
                     </div>
-                    <div class="w-full space-y-4 p-5 rounded-xl bg-white/10 backdrop-blur-md ring-1 ring-white/10 shadow-lg animate-pulse">
-                        <div class="h-6 bg-gray-200 rounded w-1/3"></div>
-                        <div class="flex flex-wrap gap-2">
-                            <div class="bg-gray-100/50 text-white text-sm px-3 py-1.5 font-medium rounded-full backdrop-blur-sm shadow hover:bg-indigo-700 transition duration-200 select-none h-5 w-20"></div>
-                            <div class="bg-gray-100/50 text-white text-sm px-3 py-1.5 font-medium rounded-full backdrop-blur-sm shadow hover:bg-indigo-700 transition duration-200 select-none h-5 w-20"></div>
-                            <div class="bg-gray-100/50 text-white text-sm px-3 py-1.5 font-medium rounded-full backdrop-blur-sm shadow hover:bg-indigo-700 transition duration-200 select-none h-5 w-20"></div>
-                            <div class="bg-gray-100/50 text-white text-sm px-3 py-1.5 font-medium rounded-full backdrop-blur-sm shadow hover:bg-indigo-700 transition duration-200 select-none h-5 w-20"></div>
-                            <div class="bg-gray-100/50 text-white text-sm px-3 py-1.5 font-medium rounded-full backdrop-blur-sm shadow hover:bg-indigo-700 transition duration-200 select-none h-5 w-20"></div>
-                            <div class="bg-gray-100/50 text-white text-sm px-3 py-1.5 font-medium rounded-full backdrop-blur-sm shadow hover:bg-indigo-700 transition duration-200 select-none h-5 w-20"></div>
-                            <div class="bg-gray-100/50 text-white text-sm px-3 py-1.5 font-medium rounded-full backdrop-blur-sm shadow hover:bg-indigo-700 transition duration-200 select-none h-5 w-20"></div>
-                            <div class="bg-gray-100/50 text-white text-sm px-3 py-1.5 font-medium rounded-full backdrop-blur-sm shadow hover:bg-indigo-700 transition duration-200 select-none h-5 w-20"></div>
-                            <div class="bg-gray-100/50 text-white text-sm px-3 py-1.5 font-medium rounded-full backdrop-blur-sm shadow hover:bg-indigo-700 transition duration-200 select-none h-5 w-20"></div>
-                            <div class="bg-gray-100/50 text-white text-sm px-3 py-1.5 font-medium rounded-full backdrop-blur-sm shadow hover:bg-indigo-700 transition duration-200 select-none h-5 w-20"></div>
-                            <div class="bg-gray-100/50 text-white text-sm px-3 py-1.5 font-medium rounded-full backdrop-blur-sm shadow hover:bg-indigo-700 transition duration-200 select-none h-5 w-20"></div>
-                            <div class="bg-gray-100/50 text-white text-sm px-3 py-1.5 font-medium rounded-full backdrop-blur-sm shadow hover:bg-indigo-700 transition duration-200 select-none h-5 w-20"></div>
+                    <div className="w-full space-y-4 p-5 rounded-xl bg-white/10 backdrop-blur-md ring-1 ring-white/10 shadow-lg animate-pulse">
+                        <div className="h-6 bg-gray-200 rounded w-1/3"></div>
+                        <div className="flex flex-wrap gap-2">
+                            <div className="bg-gray-100/50 text-white text-sm px-3 py-1.5 font-medium rounded-full backdrop-blur-sm shadow hover:bg-indigo-700 transition duration-200 select-none h-5 w-20"></div>
+                            <div className="bg-gray-100/50 text-white text-sm px-3 py-1.5 font-medium rounded-full backdrop-blur-sm shadow hover:bg-indigo-700 transition duration-200 select-none h-5 w-20"></div>
+                            <div className="bg-gray-100/50 text-white text-sm px-3 py-1.5 font-medium rounded-full backdrop-blur-sm shadow hover:bg-indigo-700 transition duration-200 select-none h-5 w-20"></div>
+                            <div className="bg-gray-100/50 text-white text-sm px-3 py-1.5 font-medium rounded-full backdrop-blur-sm shadow hover:bg-indigo-700 transition duration-200 select-none h-5 w-20"></div>
+                            <div className="bg-gray-100/50 text-white text-sm px-3 py-1.5 font-medium rounded-full backdrop-blur-sm shadow hover:bg-indigo-700 transition duration-200 select-none h-5 w-20"></div>
+                            <div className="bg-gray-100/50 text-white text-sm px-3 py-1.5 font-medium rounded-full backdrop-blur-sm shadow hover:bg-indigo-700 transition duration-200 select-none h-5 w-20"></div>
+                            <div className="bg-gray-100/50 text-white text-sm px-3 py-1.5 font-medium rounded-full backdrop-blur-sm shadow hover:bg-indigo-700 transition duration-200 select-none h-5 w-20"></div>
+                            <div className="bg-gray-100/50 text-white text-sm px-3 py-1.5 font-medium rounded-full backdrop-blur-sm shadow hover:bg-indigo-700 transition duration-200 select-none h-5 w-20"></div>
+                            <div className="bg-gray-100/50 text-white text-sm px-3 py-1.5 font-medium rounded-full backdrop-blur-sm shadow hover:bg-indigo-700 transition duration-200 select-none h-5 w-20"></div>
+                            <div className="bg-gray-100/50 text-white text-sm px-3 py-1.5 font-medium rounded-full backdrop-blur-sm shadow hover:bg-indigo-700 transition duration-200 select-none h-5 w-20"></div>
+                            <div className="bg-gray-100/50 text-white text-sm px-3 py-1.5 font-medium rounded-full backdrop-blur-sm shadow hover:bg-indigo-700 transition duration-200 select-none h-5 w-20"></div>
+                            <div className="bg-gray-100/50 text-white text-sm px-3 py-1.5 font-medium rounded-full backdrop-blur-sm shadow hover:bg-indigo-700 transition duration-200 select-none h-5 w-20"></div>
                         </div>
                     </div>
-                    <div class="w-full space-y-4 p-5 rounded-xl bg-white/10 backdrop-blur-md ring-1 ring-white/10 shadow-lg animate-pulse">
-                        <div class="h-6 bg-gray-200 rounded w-1/3"></div>
-                        <div class="flex flex-wrap gap-2">
-                            <div class="bg-gray-100/50 text-white text-sm px-3 py-1.5 font-medium rounded-full backdrop-blur-sm shadow hover:bg-indigo-700 transition duration-200 select-none h-5 w-20"></div>
-                            <div class="bg-gray-100/50 text-white text-sm px-3 py-1.5 font-medium rounded-full backdrop-blur-sm shadow hover:bg-indigo-700 transition duration-200 select-none h-5 w-20"></div>
-                            <div class="bg-gray-100/50 text-white text-sm px-3 py-1.5 font-medium rounded-full backdrop-blur-sm shadow hover:bg-indigo-700 transition duration-200 select-none h-5 w-20"></div>
-                            <div class="bg-gray-100/50 text-white text-sm px-3 py-1.5 font-medium rounded-full backdrop-blur-sm shadow hover:bg-indigo-700 transition duration-200 select-none h-5 w-20"></div>
-                            <div class="bg-gray-100/50 text-white text-sm px-3 py-1.5 font-medium rounded-full backdrop-blur-sm shadow hover:bg-indigo-700 transition duration-200 select-none h-5 w-20"></div>
-                            <div class="bg-gray-100/50 text-white text-sm px-3 py-1.5 font-medium rounded-full backdrop-blur-sm shadow hover:bg-indigo-700 transition duration-200 select-none h-5 w-20"></div>
-                            <div class="bg-gray-100/50 text-white text-sm px-3 py-1.5 font-medium rounded-full backdrop-blur-sm shadow hover:bg-indigo-700 transition duration-200 select-none h-5 w-20"></div>
-                            <div class="bg-gray-100/50 text-white text-sm px-3 py-1.5 font-medium rounded-full backdrop-blur-sm shadow hover:bg-indigo-700 transition duration-200 select-none h-5 w-20"></div>
-                            <div class="bg-gray-100/50 text-white text-sm px-3 py-1.5 font-medium rounded-full backdrop-blur-sm shadow hover:bg-indigo-700 transition duration-200 select-none h-5 w-20"></div>
-                            <div class="bg-gray-100/50 text-white text-sm px-3 py-1.5 font-medium rounded-full backdrop-blur-sm shadow hover:bg-indigo-700 transition duration-200 select-none h-5 w-20"></div>
-                            <div class="bg-gray-100/50 text-white text-sm px-3 py-1.5 font-medium rounded-full backdrop-blur-sm shadow hover:bg-indigo-700 transition duration-200 select-none h-5 w-20"></div>
-                            <div class="bg-gray-100/50 text-white text-sm px-3 py-1.5 font-medium rounded-full backdrop-blur-sm shadow hover:bg-indigo-700 transition duration-200 select-none h-5 w-20"></div>
+                    <div className="w-full space-y-4 p-5 rounded-xl bg-white/10 backdrop-blur-md ring-1 ring-white/10 shadow-lg animate-pulse">
+                        <div className="h-6 bg-gray-200 rounded w-1/3"></div>
+                        <div className="flex flex-wrap gap-2">
+                            <div className="bg-gray-100/50 text-white text-sm px-3 py-1.5 font-medium rounded-full backdrop-blur-sm shadow hover:bg-indigo-700 transition duration-200 select-none h-5 w-20"></div>
+                            <div className="bg-gray-100/50 text-white text-sm px-3 py-1.5 font-medium rounded-full backdrop-blur-sm shadow hover:bg-indigo-700 transition duration-200 select-none h-5 w-20"></div>
+                            <div className="bg-gray-100/50 text-white text-sm px-3 py-1.5 font-medium rounded-full backdrop-blur-sm shadow hover:bg-indigo-700 transition duration-200 select-none h-5 w-20"></div>
+                            <div className="bg-gray-100/50 text-white text-sm px-3 py-1.5 font-medium rounded-full backdrop-blur-sm shadow hover:bg-indigo-700 transition duration-200 select-none h-5 w-20"></div>
+                            <div className="bg-gray-100/50 text-white text-sm px-3 py-1.5 font-medium rounded-full backdrop-blur-sm shadow hover:bg-indigo-700 transition duration-200 select-none h-5 w-20"></div>
+                            <div className="bg-gray-100/50 text-white text-sm px-3 py-1.5 font-medium rounded-full backdrop-blur-sm shadow hover:bg-indigo-700 transition duration-200 select-none h-5 w-20"></div>
+                            <div className="bg-gray-100/50 text-white text-sm px-3 py-1.5 font-medium rounded-full backdrop-blur-sm shadow hover:bg-indigo-700 transition duration-200 select-none h-5 w-20"></div>
+                            <div className="bg-gray-100/50 text-white text-sm px-3 py-1.5 font-medium rounded-full backdrop-blur-sm shadow hover:bg-indigo-700 transition duration-200 select-none h-5 w-20"></div>
+                            <div className="bg-gray-100/50 text-white text-sm px-3 py-1.5 font-medium rounded-full backdrop-blur-sm shadow hover:bg-indigo-700 transition duration-200 select-none h-5 w-20"></div>
+                            <div className="bg-gray-100/50 text-white text-sm px-3 py-1.5 font-medium rounded-full backdrop-blur-sm shadow hover:bg-indigo-700 transition duration-200 select-none h-5 w-20"></div>
+                            <div className="bg-gray-100/50 text-white text-sm px-3 py-1.5 font-medium rounded-full backdrop-blur-sm shadow hover:bg-indigo-700 transition duration-200 select-none h-5 w-20"></div>
+                            <div className="bg-gray-100/50 text-white text-sm px-3 py-1.5 font-medium rounded-full backdrop-blur-sm shadow hover:bg-indigo-700 transition duration-200 select-none h-5 w-20"></div>
                         </div>
                     </div>
                 </div>
