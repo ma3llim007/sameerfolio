a36360f82c3747fb253c1a5dd9ac9f836ad501c1
ma3llim007 <mohdsameer68257@gmail.com>
2025-04-14 23:01:04 +0530

    refactor: update heading font; add border and refactor skills

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -1,21 +1,21 @@
 import Container from "./Container";
 
-const frontEndSkills = ["HTML", "CSS", "Bootstrap", "TailwindCss", "JavaScript", "ReactJs", "State Managements", "React Router", "Redux / Zustand / Context API", "Response Design"];
-const backendSkills = ["Node.js", "Express.js", "MongoDB", "Mongoose (ODM)", "RESTful APIs", "API Versioning", "Authentication (JWT)", "SQL", "Middleware"];
-const toolsSkills = ["Git & GitHub", "VS Code", "Postman", "NPM / Yarn", "ESLint", "Prettier", "Chrome DevTools", "Figma (Basic)", "Linux CLI (Basics)"];
+const frontEndSkills = ["HTML", "CSS", "Bootstrap", "TailwindCss", "JavaScript", "ReactJs", "React Router", "State Management", "Response Design"];
+const backendSkills = ["Node.js", "Express.js", "MongoDB", "Mongoose", "RESTful APIs", "API Versioning", "Authentication (JWT)", "SQL", "Middleware"];
+const toolsSkills = ["Git & GitHub", "VS Code", "Postman", "NPM / Yarn", "ESLint", "Prettier", "Chrome DevTools", "Figma", "Docker"];
 const Skills = () => {
     return (
         <section className="w-full py-7" id="skills">
             <Container>
-                <h3 className="text-2xl font-extrabold mb-3 underline">What I Know</h3>
+                <h2 className="text-3xl lg:text-4xl font-extrabold mb-6 underline underline-offset-4 font-playfair">Tech Stack</h2>
                 <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
                     <div className="w-full space-y-4 p-5 rounded-xl bg-white/10 backdrop-blur-md ring-1 ring-white/10 shadow-lg">
-                        <h4 className="text-xl font-bold text-white border-b border-white/20 pb-2">Front-End</h4>
+                        <h4 className="text-2xl font-bold text-white border-b border-white/20 pb-1 font-playfair">Front-End</h4>
                         <div className="flex flex-wrap gap-2">
                             {frontEndSkills?.map((tech) => (
                                 <span
                                     key={tech}
-                                    className="bg-gray-100/30 text-white text-sm px-3 py-1.5 font-medium rounded-full backdrop-blur-sm shadow hover:bg-indigo-700 transition duration-200 select-none"
+                                    className="bg-gray-100/30 text-white text-sm px-3 py-1.5 font-medium rounded-md border border-indigo-700 hover:border-white backdrop-blur-sm shadow hover:bg-indigo-700 transition duration-200 select-none"
                                 >
                                     {tech}
                                 </span>
@@ -23,12 +23,12 @@ const Skills = () => {
                         </div>
                     </div>
                     <div className="w-full space-y-4 p-5 rounded-xl bg-white/10 backdrop-blur-md ring-1 ring-white/10 shadow-lg">
-                        <h4 className="text-xl font-bold text-white border-b border-white/20 pb-2">Back-End</h4>
+                        <h4 className="text-2xl font-bold text-white border-b border-white/20 pb-1 font-playfair">Back-End</h4>
                         <div className="flex flex-wrap gap-2">
                             {backendSkills?.map((tech) => (
                                 <span
                                     key={tech}
-                                    className="bg-gray-100/30 text-white text-sm px-3 py-1.5 font-medium rounded-full backdrop-blur-sm shadow hover:bg-indigo-700 transition duration-200 select-none"
+                                    className="bg-gray-100/30 text-white text-sm px-3 py-1.5 font-medium rounded-md border border-indigo-700 hover:border-white backdrop-blur-sm shadow hover:bg-indigo-700 transition duration-200 select-none"
                                 >
                                     {tech}
                                 </span>
@@ -36,12 +36,12 @@ const Skills = () => {
                         </div>
                     </div>
                     <div className="w-full space-y-4 p-5 rounded-xl bg-white/10 backdrop-blur-md ring-1 ring-white/10 shadow-lg">
-                        <h4 className="text-xl font-bold text-white border-b border-white/20 pb-2">Tools</h4>
+                        <h4 className="text-2xl font-bold text-white border-b border-white/20 pb-1 font-playfair">Tools</h4>
                         <div className="flex flex-wrap gap-2">
                             {toolsSkills?.map((tech) => (
                                 <span
                                     key={tech}
-                                    className="bg-gray-100/30 text-white text-sm px-3 py-1.5 font-medium rounded-full backdrop-blur-sm shadow hover:bg-indigo-700 transition duration-200 select-none"
+                                    className="bg-gray-100/30 text-white text-sm px-3 py-1.5 font-medium rounded-md border border-indigo-700 hover:border-white backdrop-blur-sm shadow hover:bg-indigo-700 transition duration-200 select-none"
                                 >
                                     {tech}
                                 </span>
