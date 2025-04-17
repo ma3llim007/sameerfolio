9cf2db0dd8c02d9f03f165ea1bb131950c5b8ec6
ma3llim007 <mohdsameer68257@gmail.com>
2025-04-17 17:48:35 +0530

    style: improve card layout

--- src/components/Education.jsx
+++ src/components/Education.jsx
@@ -36,11 +36,11 @@ const data = {
 };
 
 const TimelineItem = ({ title, subtitle, date, description }) => (
-    <div className="relative pl-4 border-l-2 border-blue-600 pb-10">
-        <div className="absolute -left-[10px] top-5 w-3.5 h-3.5 bg-blue-600 rounded-full shadow-lg ring-4 dark:ring-gray-200"></div>
+    <div className="relative pl-4 border-l-2 border-blue-600 pb-4">
+        <div className="absolute -left-[9px] top-5 w-3.5 h-3.5 bg-blue-600 rounded-full shadow-lg ring-4 dark:ring-gray-200"></div>
         <div className="bg-white/20 backdrop-blur-xl rounded-lg shadow-md hover:shadow-xl transition-all duration-300 p-4 hover:scale-[1.01] hover:shadow-indigo-500/30">
-            <h4 className="text-lg font-semibold">{title}</h4>
-            <p className="text-sm mb-1">{subtitle}</p>
+            <h4 className="text-lg font-bold">{title}</h4>
+            <p className="text-sm mb-1 font-semibold">{subtitle}</p>
             <p className="text-xs mb-2">{date}</p>
             <p className="text-sm leading-relaxed">{description}</p>
         </div>
