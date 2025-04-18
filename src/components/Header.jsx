f7f7901b12af2888d07de180dd041aa2936f284e
ma3llim007 <mohdsameer68257@gmail.com>
2025-04-18 17:00:58 +0530

    style: improve the padding of header in y-axis

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -8,7 +8,7 @@ const Header = () => {
     const [isOpen, setIsOpen] = useState(false);
     return (
         <>
-            <div className="fixed top-0 left-0 right-0 z-50 bg-indigo-900/80 backdrop-blur-md border-b border-white/10 transition-all duration-300 shadow-xl">
+            <div className="fixed top-0 left-0 right-0 z-50 py-2 bg-indigo-900/80 backdrop-blur-md border-b border-white/10 transition-all duration-300 shadow-xl">
                 <Container>
                     <div className="grid grid-cols-2 items-center">
                         {/* Logo Section */}
@@ -16,7 +16,7 @@ const Header = () => {
                             <img className="w-[250px] h-[150px] object-contain" src="logo.svg" alt="sameerfolio Logo" />
                         </Link>
                         <nav className="flex items-center justify-end gap-6">
-                            <ul className="hidden md:flex items-center gap-6 font-semibold text-lg">
+                            <ul className="hidden md:flex items-center gap-6 text-lg">
                                 <li className="hover:text-primary transition">
                                     <Link to="home" smooth={true} duration={500} className="select-none cursor-pointer">
                                         Home
