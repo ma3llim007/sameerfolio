d0a19f9c697c376b87acf34c94a1e4722575505d
ma3llim007 <mohdsameer68257@gmail.com>
2025-09-21 11:22:11 +0530

    chore: add google analytics in site

--- src/App.jsx
+++ src/App.jsx
@@ -5,8 +5,14 @@ import Contact from "./components/Contact";
 import Footer from "./components/Footer";
 import Hero from "./components/Hero";
 import Skills from "./components/Skills";
+import ReactGA from "react-ga4";
+import { useEffect } from "react";
 
 const App = () => {
+    useEffect(() => {
+        ReactGA.initialize(import.meta.env.VITE_GOOGLEANALYTICS);
+        ReactGA.send({ hitType: "pageview", page: window.location.pathname, title: "App.jsx" });
+    }, []);
     return (
         <div className="w-full min-h-screen bg-[#020817] text-white font-roboto scroll-smooth">
             <Header />
